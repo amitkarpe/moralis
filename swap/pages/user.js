fdd41f086f38c0d7ca7bdeaae692937e255dc31f
Amit Karpe <amitkarpe@gmail.com>
2023-01-07 12:31:15 +0000

    Added: Multiple token in the loop. FAiled.

--- swap/pages/user.js
+++ swap/pages/user.js
@@ -4,6 +4,20 @@ import { useState } from "react";
 import axios from "axios";
 import { useSendTransaction } from "wagmi";
 
+let MATIC = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+let USDC  = '0x2791bca1f2de4661ed88a30c99a7a9449aa84174'
+let USDT  = '0xc2132d05d31c914a87c6611c10748aeb04b58e8f'
+let DAI   = '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063'
+let buyToken;
+const fund = new Map([
+    ['FTM','0xb85517b87bf64942adf3a0b9e4c71e4bc5caa4e5'],
+    ['ONE','0x80c0cbdb8d0b190238795d376f0bd57fd40525f2'],
+    ['ATOM','0xac51C4c48Dc3116487eD4BC16542e27B5694Da1b'],
+    ['NEAR','0x72bd80445b0db58ebe3e8db056529d4c5faf6f2f'],
+  ]);
+  
+
+
 function User({ user, balance }) {
   const [fromToken] = useState("0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE");
   const [toToken, setToToken] = useState(
@@ -15,6 +29,14 @@ function User({ user, balance }) {
   const [to, setTo] = useState("");
   const [txData, setTxData] = useState("");
 
+  // async function get1inchSwap(toToken,tokenValue){
+  //   const tx = await axios.get(`https://api.1inch.io/v4.0/137/swap?fromTokenAddress=${fromToken}&toTokenAddress=${toToken}&amount=${tokenValue}&fromAddress=${user.address}&slippage=5`);    
+  //   console.log(tx.data)
+  //   setTo(tx.data.tx.to);
+  //   setTxData(tx.data.tx.data);
+  //   setValueExchangedDecimals(Number(`1E${tx.data.toToken.decimals}`));
+  //   setValueExchanged(tx.data.toTokenAmount);
+  //   }
 
   const { data, isLoading, isSuccess, sendTransaction } = useSendTransaction({
       request: {
@@ -23,8 +45,51 @@ function User({ user, balance }) {
           data: String(txData),
           value: String(value),
       },
-})
+  })
+ 
+  async function processBuy(toToken,tokenValue){
+    console.log("value==>",tokenValue);
+    // fund.forEach {
+    //     console.log(key + ' = ' + tokenAddress);
+    //     // console.log(parseFloat(value)/fund.size);
+    //     // get1inchSwap(tokenAddress,parseFloat(value)/fund.size);
+    //     var receipt = await processBuy(tokenAddress, parseFloat(value) / fund.size);
+    //   })
+      
+      let processOne = new Promise((resolve, reject) => {
+        try {
+            const tx = axios.get(`https://api.1inch.io/v4.0/137/swap?fromTokenAddress=${fromToken}&toTokenAddress=${toToken}&amount=${tokenValue}&fromAddress=${user.address}&slippage=5`);    
+            console.log(tx.data)
+            setTo(tx.data.tx.to);
+            setTxData(tx.data.tx.data);
+            setValueExchangedDecimals(Number(`1E${tx.data.toToken.decimals}`));
+            setValueExchanged(tx.data.toTokenAmount);
 
+            sendTransaction();
+            console.log(data);
+            if (!isSuccess) {
+              setTimeout(() => resolve(toToken, 2000));
+            }
+            
+          }
+        catch (error) {
+            console.log(error);
+        }
+
+        });
+      let result = await processOne;
+      console.log(result);
+      };
+
+    
+
+  async function invest(){
+    fund.forEach ((tokenAddress, key) => {
+        console.log(key + ' = ' + tokenAddress);
+        var receipt = processBuy(tokenAddress, parseFloat(value) / fund.size);
+      })
+  }
+ 
 
   function changeToToken(e){
     setToToken(e.target.value);
@@ -35,16 +100,6 @@ function User({ user, balance }) {
     setValue(e.target.value * 1E18);
     setValueExchanged("");
   }
-
-  async function get1inchSwap(){
-    const tx = await axios.get(`https://api.1inch.io/v4.0/137/swap?fromTokenAddress=${fromToken}&toTokenAddress=${toToken}&amount=${value}&fromAddress=${user.address}&slippage=5`);    
-    console.log(tx.data)
-    setTo(tx.data.tx.to);
-    setTxData(tx.data.tx.data);
-    setValueExchangedDecimals(Number(`1E${tx.data.toToken.decimals}`));
-    setValueExchanged(tx.data.toTokenAmount);
-    }
-
 
 
   return (
@@ -79,8 +134,9 @@ function User({ user, balance }) {
       ></input>
       <br />
       <br />
-      <button onClick={get1inchSwap}>Get Conversion</button>
-      <button disabled={!valueExchanged} onClick={sendTransaction}>Swap Tokens</button>
+      <button onClick={invest}>invest</button>
+      {/* <button onClick={get1inchSwap}>Get Conversion</button>
+      <button disabled={!valueExchanged} onClick={sendTransaction}>Swap Tokens</button> */}
       {isLoading && <div>Check Wallet</div>}
       {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
       <br />
